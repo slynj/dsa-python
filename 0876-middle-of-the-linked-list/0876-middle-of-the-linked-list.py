5d5ce6e4f245304c5f0af03080e8fd2bdade0836
Lyn Jeong <38800761+slynj@users.noreply.github.com>
2024-07-11 07:51:49 -0400

    Time: 31 ms (80.98%), Space: 16.4 MB (92.26%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,35 +5,13 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        new = ListNode()
-        len = 1
+        # 두칸씩 가면 원래애의 중간지점을 찾을수있으니까
+        old = head
+        new = head
 
-        cur_node = head
-        while (cur_node.next != None):
-            len += 1
-            cur_node = cur_node.next
-        
-        index = len / 2 if (len % 2 == 0) else floor(len / 2)
+        while (old and old.next):
+            old = old.next.next
+            new = new.next
 
-        starting = new
-        original = head
-
-        for i in range (len + 1):
-            if i == index:
-                starting.val = original.val
-                starting = starting.next
-
-            elif i > index:
-                starting = ListNode(original.val, None)
-                starting = starting.next
-                print(index)
-
-            original = original.next
-
-            # if original == None:
-            #     starting = None
-            #     break
-        
-            
         return new
         
