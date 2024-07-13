8039c1fbac287fd7066c17ab635dab2450443053
Lyn Jeong <38800761+slynj@users.noreply.github.com>
2024-07-13 15:54:05 -0400

    Update 0088-merge-sorted-array-optimal.py

--- 0088-merge-sorted-array/0088-merge-sorted-array-optimal.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array-optimal.py
@@ -17,33 +17,5 @@ class Solution:
                 nums1[i] = nums2[i2]
                 i2 -= 1
             i -=1
-
-        # index1 = m - 1 #if m != 0 else m
-        # index2 = n - 1 #if n != 0 else n
-        # current = m + n - 1
-        # if m == 0:
-        #     for i in range(n):
-        #         nums1[i] = nums2[i]
-        # else:
-        #     for i in range(m + n):
-                
-        #         if index2 < 0:
-        #             break
-        #         if index1 < 0:
-        #             if current == 0:
-        #                 nums1[current] = nums2[index2]
-        #                 break
-
-        #             nums1[current] = nums2[index2]
-        #             index2 -= 1
-        #         else:
-        #             if nums1[index1] >= nums2[index2]:
-        #                 nums1[current] = nums1[index1]
-        #                 index1 -= 1
-        #             else:
-        #                 nums1[current] = nums2[index2]
-        #                 index2 -= 1
-        #         current -= 1
-
                 
                 
