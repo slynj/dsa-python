a62f50e8ea091d21b36dd5e31da95aa40c8fcf1d
Lyn Jeong <38800761+slynj@users.noreply.github.com>
2024-07-16 11:01:38 -0400

    Time: 162 ms (88.98%), Space: 18.2 MB (14.3%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -7,20 +7,13 @@ class Solution:
         if length == 1 or length == 2:
             return nums[0]
 
-        if nums[0] == nums[n - 1] and (n - 1 < length):
+        if nums[0] == nums[n - 1]:
             return nums[0]
+        
         for i in range(1, length):
             if nums[i - 1] != nums[i]:
 
                 if nums[i] == nums[i + n - 1] and (i + n - 1 < length):
                 
                     return nums[i]
-
-        # for num in nums:
-        #     counter = 0
-        #     for num2 in nums:
-        #         if num == num2:
-        #             counter += 1
-        #         if counter >= n:
-        #             return num
 
